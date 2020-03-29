92763ee5a82a2d75d591779b171fffeae0126529
9Sefa9 <sefagoevercin@hotmail.de>
2020-03-30 00:53:35 +0200

    Erste Überlegungen zu Links Rechts Column Layout gesammelt bzw. die Methode geschrieben(PDFSTripperByArea..) um links rechts unterscheiden zu können.

--- src/main/java/extractor/PLAYGROUND.java
+++ src/main/java/extractor/PLAYGROUND.java
@@ -5,8 +5,10 @@ import org.apache.fontbox.util.BoundingBox;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageTree;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
 import org.apache.pdfbox.text.TextPosition;
 import utilities.Helper;
 
@@ -34,12 +36,74 @@ public class PLAYGROUND {
     public void start() throws IOException {
 
         loadPDFDocument(pdfPath);
-        analyzePDFDocument();
-        Helper.csvWriter("G:/Users/Progamer/Desktop/addressesWrite.csv",wordsAndOccurencesMap);
-      //  printCompleteDoc();
+        // analyzePDFDocument();
+        // Helper.csvWriter("G:/Users/Progamer/Desktop/addressesWrite.csv", wordsAndOccurencesMap);
+        //  printCompleteDoc();
         //drawCharactersBoundingBox();
-      //  startPDF();
+        //  startPDF();
+        linksRechtsSectionBoundingArea();
+    }
+    //Überlegung:  Es existieren wörter, die in der Mitte kein Sinn ergeben. z.b But:  Bu    t
+    //ERste Vorüberlegung wäre: wenn die anzahl an brüchigen Wörtern ab einem bestimmten Faktor hoch sind => zwei abschnitte vorhanden.
+    //Ansonsten handelt es sich um Fließtext.
+    //Ich könnte aber auch teorethisch untersuchen ob es silben trennungen gibt wie :   pedestri-
+    //keine ahnung. Muss ich noch mal schauen...
+    private void linksRechtsSectionBoundingArea() {
+       /* float width = this.pdfDocument.getPage(0).getMediaBox().getWidth();
+        float height= this.pdfDocument.getPage(0).getMediaBox().getHeight();
+        Rectangle2D region = new Rectangle2D.Double(100,100, 100,100);
+        String regionName = "region";
+        PDFTextStripperByArea stripper = null;
+        try {
+            stripper = new PDFTextStripperByArea();
+            stripper.setSortByPosition(false);
+            stripper.addRegion(regionName, region);
+            stripper.extractRegions(this.pdfDocument.getPage(0));
+            this.pdfDocumentText = stripper.getText(this.pdfDocument);
+            System.out.println(stripper.getTextForRegion(regionName));
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        */
+        try {
+            float width = this.pdfDocument.getPage(0).getMediaBox().getWidth();
+            float height= this.pdfDocument.getPage(0).getMediaBox().getHeight();
+            StringBuilder pdfText = new StringBuilder();
+            PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+
+            stripper.setSortByPosition(true);
+
+            Rectangle2D rectLeft = new Rectangle2D.Float(0, 0, width/2,height);
+
+            Rectangle2D rectRight = new Rectangle2D.Float(width/2,0, width/2, height);
+
+            stripper.addRegion("leftColumn", rectLeft);
+
+            stripper.addRegion("rightColumn", rectRight);
+
+            PDPageTree allPages = this.pdfDocument.getDocumentCatalog().getPages();
+            int pageNumber = this.pdfDocument.getNumberOfPages();
+
+
+            String leftText = "";
+            String rightText = "";
+
+            for (int i = 0; i < pageNumber; i++) {
+
+                PDPage page = allPages.get(i);
+
+                stripper.extractRegions(page);
+                leftText = stripper.getTextForRegion("leftColumn");
+                rightText = stripper.getTextForRegion("rightColumn");
+
+                pdfText.append(leftText);
+                pdfText.append(rightText);
+            }
+            System.out.println(pdfText.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -61,37 +125,37 @@ public class PLAYGROUND {
     }
 
     //Eventuell abschnitte rausfiltern, anzahl buchstaben etc filtern.. usw. Mit zusammenarbeit von Data.java
+    //hier werden zudem charactersBoxCoordinatesMap gefüllt um es später mit der Methode drawCharactersBoundingBox zeichnen zu können.
     private void analyzePDFDocument() {
         try {
-            pdfTextStripper = new PDFTextStripper(){
+            pdfTextStripper = new PDFTextStripper() {
                 @Override
                 protected void writeString(String text, List<TextPosition> textPositions) throws IOException {
                     pdfTextStripper.setParagraphStart("\t");
                     StringBuilder builder = new StringBuilder();
                     //Wird zum erstellen eines csv Files verwendet.
                     //\w matched alle char's die kein word sind. any non-word character.
-                            wordsAndOccurencesMap.merge(text.replaceAll("\\W","").toLowerCase(Locale.ENGLISH),1,Integer::sum);
-                    for(TextPosition t : textPositions){
-                            //Zum Umranden der einzelnen Character.
-                            PDFont font = t.getFont();
-                            BoundingBox boundingBox = font.getBoundingBox();
-                            Rectangle2D.Float rect = new Rectangle2D.Float(boundingBox.getLowerLeftX(), boundingBox.getUpperRightY(), boundingBox.getWidth(), boundingBox.getHeight());
-                            AffineTransform affineTransform = t.getTextMatrix().createAffineTransform();
-                            affineTransform.scale(1 / 1000f, 1 / 1000f);
-                            Shape shape = affineTransform.createTransformedShape(rect);
-                            // Invertierung der Y - Achse.  Notwendig, da java in 2D User Space arbeitet. Und nicht in PDF User Space.
-                            Rectangle2D bounds = shape.getBounds2D();
-                            bounds.setRect(bounds.getX(), bounds.getY() - bounds.getHeight(), bounds.getWidth(), bounds.getHeight());
+                    wordsAndOccurencesMap.merge(text.replaceAll("\\W", "").toLowerCase(Locale.ENGLISH), 1, Integer::sum);
+                    for (TextPosition t : textPositions) {
+                        //Zum Umranden der einzelnen Character.
+                        PDFont font = t.getFont();
+                        BoundingBox boundingBox = font.getBoundingBox();
+                        Rectangle2D.Float rect = new Rectangle2D.Float(boundingBox.getLowerLeftX(), boundingBox.getUpperRightY(), boundingBox.getWidth(), boundingBox.getHeight());
+                        AffineTransform affineTransform = t.getTextMatrix().createAffineTransform();
+                        affineTransform.scale(1 / 1000f, 1 / 1000f);
+                        Shape shape = affineTransform.createTransformedShape(rect);
+                        // Invertierung der Y - Achse.  Notwendig, da java in 2D User Space arbeitet. Und nicht in PDF User Space.
+                        Rectangle2D bounds = shape.getBounds2D();
+                        bounds.setRect(bounds.getX(), bounds.getY() - bounds.getHeight(), bounds.getWidth(), bounds.getHeight());
 
-                            //Wird zum zeichnen verwendet.
-                            charactersBoxCoordinatesMap.put(bounds, getCurrentPageNo());
+                        //Wird zum zeichnen verwendet.
+                        charactersBoxCoordinatesMap.put(bounds, getCurrentPageNo());
 
 
-
-                        if(builder.length()==0){
-                           // builder.append("[[[FontSize:"+t.getFontSizeInPt()+" || "+t.getPageWidth()+ "]]]  ");
+                        if (builder.length() == 0) {
+                            // builder.append("[[[FontSize:"+t.getFontSizeInPt()+" || "+t.getPageWidth()+ "]]]  ");
                         }
-                        if(!t.equals(-1)){
+                        if (!t.equals(-1)) {
                             builder.append(t);
                         }
 
@@ -108,6 +172,7 @@ public class PLAYGROUND {
             e.printStackTrace();
         }
     }
+
     private void loadPDFDocument(String pdfPath) {
         String absolutePath;
         try {
@@ -119,22 +184,22 @@ public class PLAYGROUND {
 
     }
 
-    private void printCompleteDoc(){
+    private void printCompleteDoc() {
         if (!pdfDocument.isEncrypted()) {
             for (String line : this.pdfDocumentText.split(pdfTextStripper.getParagraphStart())) {
                 String[] words = line.split(" ");
                 String firstWord = words[0].trim();
-                String lastWord = words[words.length-1].trim();
-                System.out.println("FirstWord:"+firstWord);
+                String lastWord = words[words.length - 1].trim();
+                System.out.println("FirstWord:" + firstWord);
 
 
                 //Diese If Bedingung sorgt dafür, dass z.B "localization\nof" gesplittet wird.
-                if(lastWord.contains("\n")){
-                   String[] lastWordCorrection = lastWord.split("\n");
-                   lastWord = lastWordCorrection[lastWordCorrection.length-1];
+                if (lastWord.contains("\n")) {
+                    String[] lastWordCorrection = lastWord.split("\n");
+                    lastWord = lastWordCorrection[lastWordCorrection.length - 1];
                 }
 
-                System.out.println("lastWord:"+lastWord);
+                System.out.println("lastWord:" + lastWord);
 
                 //Printed den ganzen Line.
                 System.out.println(line);
@@ -142,18 +207,19 @@ public class PLAYGROUND {
             }
         }
     }
-    private void drawCharactersBoundingBox(){
-        PDPage page=null;
-        PDPageContentStream contentStream=null;
+
+    private void drawCharactersBoundingBox() {
+        PDPage page = null;
+        PDPageContentStream contentStream = null;
         try {
-            if(charactersBoxCoordinatesMap == null)
+            if (charactersBoxCoordinatesMap == null)
                 throw new DrawException("Map is NULL!");
 
-            for(Map.Entry<Rectangle2D, Integer> map : charactersBoxCoordinatesMap.entrySet()) {
+            for (Map.Entry<Rectangle2D, Integer> map : charactersBoxCoordinatesMap.entrySet()) {
 
-                page = this.pdfDocument.getPage(map.getValue()-1);
-                contentStream = new PDPageContentStream(this.pdfDocument,page,true,true);
-                contentStream.addRect((float)map.getKey().getBounds2D().getX(),(float)map.getKey().getBounds2D().getY(), (float)map.getKey().getWidth(),(float)map.getKey().getHeight());
+                page = this.pdfDocument.getPage(map.getValue() - 1);
+                contentStream = new PDPageContentStream(this.pdfDocument, page, true, true);
+                contentStream.addRect((float) map.getKey().getBounds2D().getX(), (float) map.getKey().getBounds2D().getY(), (float) map.getKey().getWidth(), (float) map.getKey().getHeight());
                 contentStream.setStrokingColor(Color.RED);
                 contentStream.stroke();
                 contentStream.close();
@@ -162,7 +228,7 @@ public class PLAYGROUND {
                 File file1 = new File("src\\main\\resources\\colored08662658.pdf");
                 pdfDocument.save(file1);
                 pdfDocument.close();
-            }catch (FileNotFoundException f){
+            } catch (FileNotFoundException f) {
                 Runtime.getRuntime().exec("taskkill /F /IM AcroRd32.exe");
                 try {
                     Thread.sleep(1500);
