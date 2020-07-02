7f58df0006f52558af9bc79872f3f1bc7f02babc
9Sefa9 <sefagoevercin@hotmail.de>
2020-07-02 23:50:19 +0200

    Import fertig. Arbeite jetzt am Section.

--- src/main/java/io/Import.java
+++ src/main/java/io/Import.java
@@ -4,14 +4,21 @@ import exception.ImportException;
 import extractor.Document;
 import extractor.DocumentHandler;
 import interfaces.Extractable;
+import utilities.KeyValueObject;
 
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Import extends Extractable {
-    List<File> temp = new ArrayList<File>();
+    //Alle PDF Dateien werden in detectedFiles abgespeichert
+    ArrayList<KeyValueObject<String,File>> detectedFiles = new ArrayList<>();
+
+
+    //Anzahl an PDF Dokumenten
+    int countConferences=0;
     /**
      * PDF Dockumente werden importiert und unter der Klasse Document in eine List abgespeichert.
      *
@@ -20,23 +27,66 @@ public class Import extends Extractable {
      */
     @Override
     public void importDocument(DocumentHandler handler, String dirPath) {
+        findPDFrecursiv(handler,dirPath);
+        System.out.println("Conferences: "+countConferences+" PDFs: "+detectedFiles.size());
+
+        //Überträgt ArrayList<KeyValueOBject<String,File>> zu einem File-Array,
+        // da setDocumentsList nur File[] akzeptiert.
+        File[] myPDFFiles = new File[detectedFiles.size()];
+        for(int i = 0;i<detectedFiles.size();i++)
+            myPDFFiles[i] = detectedFiles.get(0).getValue();
+
+        //setze den Ordner mit den PDF Dokumenten in die DocumentsList.
+        handler.setDocumentsList(myPDFFiles);
+
+    }
+    /**
+     * Die Methode wird in {@link #importDocument(DocumentHandler, String)} aufgerufen und
+     * durchsucht rekursiv alle tieferliegende PDF Dokumente / Ordner / Unterordner usw.
+     * @param handler Der Dokument handler
+     * @param dirPath Pfad wo sich ein PDF / Ordner / Unterordner usw. befindet
+     */
+    private void findPDFrecursiv(DocumentHandler handler, String dirPath) {
         File file;
+        String absolutePath;
         try {
-            String absolutePath = Paths.get(dirPath).toAbsolutePath().toString();
+            absolutePath = Paths.get(dirPath).toAbsolutePath().toString();
             file = new File(absolutePath);
+
             if (!file.isDirectory() && !file.exists()) {
                 throw new ImportException("Document kann nicht importiert werden!");
             }
-            if (file.getName().endsWith(".pdf")){
-                //falls nur 1 PDF Dokument:
-                temp.add(file);
-                // handler.setDocumentsList(new File[]{file});
-            }
-            else{
-                //setze den Ordner mit den PDF Dokumenten in die DocumentsList.
-                importDocument(handler, file.getAbsolutePath()+/*Hier steht datensammlung. Muss aber der nächste Name stehen.*/);
-                //   handler.setDocumentsList(file.listFiles((dir, name) -> name.endsWith(".pdf") ? true : false));
+            //falls in Gradle, nur ein PDF als Argument angegeben wurde:
+            if (file.getName().endsWith(".pdf")) {
 
+                detectedFiles.add(new KeyValueObject<>(null,file));
+            }
+            //falls in Gradle, ein Ordner als Argument angegeben wurde:
+            else if (file.isDirectory()) {
+                //Die einzelnen Konferenz-Ordner
+                for (File firstDir : file.listFiles()) {
+                    //falls eine Konferenz in wirklichkeit eine PDF Datei ist:
+                    if(firstDir.isFile() && firstDir.getName().endsWith(".pdf")) {
+                        // System.out.println(firstDir.getName()+ "Count: "+(countPDFs +=1)+ "name: "+firstDir.getName());
+                        detectedFiles.add(new KeyValueObject<>(firstDir.getName(),firstDir));
+
+                    }else if(firstDir.isDirectory()) {
+                        countConferences+=1;
+                        for (File secondDir : firstDir.listFiles()) {
+                            //Falls der Konferenz-Ordner im Konferenz-Ordner ein File ist:
+                            if (secondDir.isFile() && secondDir.getName().endsWith(".pdf")) {
+                                //    System.out.println(secondDir.getName()+ "Count: "+(countPDFs +=1)+ "name: "+firstDir.getName());
+                                detectedFiles.add(new KeyValueObject<>(firstDir.getName(),secondDir));
+
+                                //Ansonsten ist es ein Ordner, checke Rekursiv nach weiteren PDF Dateien im Ordner
+                            }else if(secondDir.isDirectory()){
+                                countConferences+=1;
+                                System.out.println("RECURSION ENTER WITH PATH "+secondDir.getPath());
+                                importDocument(handler,secondDir.getPath());
+                            }
+                        }
+                    }
+                }
             }
         } catch (ImportException e) {
             e.printStackTrace();
---
+++ src/main/java/utilities/KeyValueObject.java
@@ -0,0 +1,38 @@
+package utilities;
+public class KeyValueObject<K,V> {
+    K key;
+    V value;
+
+    public KeyValueObject() {
+
+    }
+
+    public KeyValueObject(K key, V  value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return "KeyValueObject{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+}
